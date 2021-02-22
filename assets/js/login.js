b32f9d19667557e00c1d9af721144c467ecf615e
icarus <421822990@qq.com>
2021-02-22 11:21:42 +0800

    完成了登录和注册页面

---
+++ assets/js/login.js
@@ -0,0 +1,55 @@
+$(function () {
+    $('#link-reg').on('click', function () {
+        $('.login-box').hide();
+        $('.reg-box').show();
+    });
+    $('#link-login').on('click', function () {
+        $('.login-box').show();
+        $('.reg-box').hide();
+    })
+    var form = layui.form;
+    var layer = layui.layer;
+    form.verify({
+        pwd: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val();
+            if (pwd !== value) {
+                return '两次密码不一致'
+            }
+        }
+    })
+    //监听注册表单提交事件
+    $('#form-reg').on('submit', function (e) {
+        e.preventDefault();
+        $.post('http://ajax.frontend.itheima.net/api/reguser', { username: $('#form-reg [name=username]').val(), password: $('#form-reg [name=password]').val() }, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message);
+            }
+            layer.msg('注册成功，请登录');
+            $('#link-login').click()
+        })
+    })
+    //监听登录表单提交事件
+    $('#form-login').submit(function (e) {
+        e.preventDefault();
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败')
+                }
+                layer.msg('登录成功')
+                //跳转后台主页
+                localStorage.setItem('token',res.token)
+                location.href ='/index.html';
+            }
+
+        })
+    })
+
+})
