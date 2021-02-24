f63a2ab3c52ee388060a1b1c8d605946dbc61646
icarus <421822990@qq.com>
2021-02-24 16:07:34 +0800

    完成了个人中心代码

---
+++ assets/js/user/user_info.js
@@ -0,0 +1,42 @@
+$(function () {
+    var form = layui.form;
+    form.verify({
+        nickname: function (value) {
+            if (value.length > 6) {
+                return '昵称长度必须在1-6个字符之间'
+            }
+        }
+    });
+    initUserInfo();
+    function initUserInfo() {
+        $.ajax({
+            method: 'get',
+            url: '/my/userinfo',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取用户信息失败')
+                }
+                form.val('formUserInfo', res.data)
+            }
+        })
+    }
+    $('#btnRest').on('click', function (e) {
+        e.preventDefault();
+        initUserInfo()
+    })
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault();
+        $.ajax({
+            method: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('更新用户信息失败')
+                }
+                layer.msg('更新用户信息成功')
+                window.parent.getUserInfo();
+            }
+        })
+    })
+})
